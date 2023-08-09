01513c09cd3080d385223b29f959db30aa10d73e
vaibhav090204 <141266406+vaibhav090204@users.noreply.github.com>
2023-08-09 23:15:47 +0530

    guess the no

---
+++ script.js
@@ -0,0 +1,25 @@
+const randomValue = Math.floor(Math.random() * 100) + 1;
+let attempts = 0;
+
+const guessInput = document.getElementById('guessInput');
+const guessButton = document.getElementById('guessButton');
+const resultText = document.getElementById('resultText');
+const resultTex = document.getElementById('resultTex');
+const attemptsText = document.getElementById('attemptsText');
+
+guessButton.addEventListener('click', function () {
+    attempts++;
+    attemptsText.textContent = `Number of Attempts: ${attempts}`;
+
+    const userGuess = parseInt(guessInput.value);
+
+    if (userGuess === randomValue) {
+        resultTex.textContent = `Congratulations! 🎉 You're a mastermind! The hidden number was ${randomValue}. 
+        Your skillful score: ${200 - attempts * 2}`;
+        guessButton.disabled = true;
+    } else if (userGuess > randomValue) {
+        resultText.textContent = 'Your guess is too high. Try again.';
+    } else if (userGuess < randomValue) {
+        resultText.textContent = 'Your guess is too low. Try again.';
+    }
+});
